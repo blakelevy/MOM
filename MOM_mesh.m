fae3a4cc7cbe038e57d14bfde4afd2bd0d1168fd
Blake Levy <blake.a.levy@gmail.com>
2013-04-27 14:21:53 -0500

    Added a meshing file in MOM
    meshes the difference between two circles.

---
+++ MOM_mesh.m
@@ -0,0 +1,12 @@
+%% Meshing File for right PEC cylinder
+clc;clear;
+addpath C:\Users\Blake\Documents\GitHub\MOM\distmesh
+%% import constants.m
+constants
+%% create mesh
+diameter = 10*lamb0;
+fd = inline('ddiff(dcircle(p,0,0,1),dcircle(p,0,0,.4))','p');
+% fd = @(p) ddiff(drectangle(p,-1,1,-1,1),dcircle(p,0,0,1/2));
+fh = inline('min(5*sqrt(sum(p.^2,2))-1,2)','p');
+% fh = @(p) .05+.3*dcircle(p,0,0,1/2);
+[p t] = distmesh2d(fd,fh,0.05,[-1,-1;1,1],[-1,-1;-1,1;1,-1;1,1]);
---
+++ constants.m
@@ -0,0 +1,12 @@
+% example script
+%% Project #2
+% Authors: Blake Levy and Adedayo Lawal
+%% Set up Parameters
+c = 299792458;
+mu0 = (4*pi)*1e-7;
+epsilon = 1/(mu0*c^2);
+eps0 = epsilon;
+f = 1e6; % initial frequency
+w = 2*pi*f;
+k0 = w/c;
+lamb0 = c/f;
