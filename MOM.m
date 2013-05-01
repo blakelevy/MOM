0f6fed57719599472068d9751db691df8313a144
Ashar <lawa0014@umn.edu>
2013-05-01 13:35:33 -0500

    Added Z, I and V
    preliminary code

--- MOM.m
+++ MOM.m
@@ -3,6 +3,11 @@
 clc;clear;
 constants
 M = 8; % Number of elements
+N = M+1; % Number of nodes
 r = 1; % radius of circle
 [x y theta T slope L] = mesh_circle(M,r);
 plot(x,y)
+%% Z, V and I
+Z = -1/2*eye(M,M);
+V = zeros(M,1);
+I = zeros(M,1);
