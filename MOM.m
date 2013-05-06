c196911870703178082ba8d6370e679830c0edb8
Blake Levy <blake.a.levy@gmail.com>
2013-05-06 16:05:34 -0500

    maybe working Z Matrix

--- MOM.m
+++ MOM.m
@@ -10,13 +10,13 @@ q = 2; % q-point Quadrature
 S = zeros(2,M);
 S(1,:) = x(2:end)-x(1:end-1);
 S(2,:) = y(2:end)-y(1:end-1);
-[X Y] = testpts(x(1),y(1),S(:,1),L,q);
+% [P] = testpts(x(5),y(5),S(:,5),q);
 
 %% Z, V and I
 theta1 = 0*pi/180;
 Ez0 = 1;
 [V, m] = create_input(x,y,k0, theta1, Ez0);
-plot(x,y,'r',X,Y,'+k',m(1,:),m(2,:),'ko')
+% plot(x,y,'r',P(1),P(2),'+k',m(1,:),m(2,:),'ko')
 Z = -1/2*eye(M,M);
 R = zeros(2,length(x));
 R(1,:) = x;
@@ -24,9 +24,14 @@ R(2,:) = y;
 for i = 1:M
     for j = 1:M
         if (i ~= j)
-            Z(i,j) = (k0/4*1j)*create_Z(k0, T(i), R(:,i), R(:,j));
+            Z(i,j) = (k0/4*1j)*create_Z(k0, T(i), R(:,i), R(:,j),S(:,i),S(:,j),q);
         end
     end
 end
 
 I = zeros(M,1);
+I = Z\V';
+plot(abs(I))
+
+
+
