da5b2a9fcf336059efd829714825dff571b6882d
Blake Levy <blake.a.levy@gmail.com>
2013-05-07 15:56:29 -0500

    fixed bugs

--- MOM.m
+++ MOM.m
@@ -1,24 +1,20 @@
 %% Project #3 MOM
 % Authors: Dayo Lawal and Blake Levy
 clc;clear;
-Q = [1 2 5 10]; % q-point Quadrature
+Q = [10]; % q-point Quadrature
 for G = 1:length(Q)
     q = Q(G);
     constants
-    M = 8; % Number of elements
+    M = 10; % Number of elements
     N = M+1; % Number of nodes
-    r = 1; % radius of circle
-
-    [x y theta T slope L] = mesh_circle(M,r);
-    S = zeros(2,M);
-    S(1,:) = x(2:end)-x(1:end-1);
-    S(2,:) = y(2:end)-y(1:end-1);
+    r = 4*lamb0; % radius of circle
+    [x y theta S slope L] = mesh_circle(M,r);
     % [P] = testpts(x(5),y(5),S(:,5),q);
-
     %% Z, V and I
     theta1 = 0*pi/180;
-    Ez0 = 1;
-    [V, m] = create_input(x,y,k0, theta1, Ez0);
+    Hzo = 1;
+    V = zeros(M,1);
+%     [V, m] = create_input(x,y,k0, theta1, Ez0);
     % plot(x,y,'r',P(1),P(2),'+k',m(1,:),m(2,:),'ko')
     Z = -1/2*eye(M,M);
     R = zeros(2,length(x));
@@ -27,18 +23,19 @@ for G = 1:length(Q)
     for i = 1:M
         for j = 1:M
             if (i ~= j)
-                Z(i,j) = (k0/4*1j)*create_Z(k0, T(i), R(:,i), R(:,j),S(:,i),S(:,j),q);
-            end
+                Z(i,j) = (k0/4/1j)*create_Z(k0, S(:,i)/L(i), R(:,i), R(:,j),S(:,i),S(:,j),q);
+            end  
         end
+        V(i) = create_input(k0,theta1,Hzo,R(:,i));
     end
 
     I = zeros(M,1);
-    I = Z\V';
+    I = Z\V;
     hold on
     if q == 10
-    plot(abs(I),'k')
+    plot(abs(I./V),'k')
     else
-       plot(abs(I))
+       plot(abs(I./V))
     end
 end
 
--- create_input.m
+++ create_input.m
@@ -1,7 +1,9 @@
-function [ V, m ] = create_input(X, Y, k, theta, Ezo)
+function [ V ] = create_input(k,theta,Hzo,R)
 %create_input Summary of this function goes here
 %   Detailed explanation goes here
-m = [((X(2:end)- X(1:end-1))./2)+X(1:end-1);((Y(2:end)-Y(1:end-1))./2)+Y(1:end-1)];
-V = Ezo*exp(-1j*(k*cos(theta)*m(1,1:end)+k*sin(theta)*m(2,1:end)));
+K = [k*cos(theta) k*sin(theta)];
+r = K*R;
+% m = [((X(2:end)- X(1:end-1))./2)+X(1:end-1);((Y(2:end)-Y(1:end-1))./2)+Y(1:end-1)];
+V = Hzo*exp(-1j*r);
 end
 
