20d9281d70b6cec23e91d6a6f774f5b12d903f7a
Blake Levy <blake.a.levy@gmail.com>
2013-05-10 15:44:58 -0500

    Created Z_notes
    Changed some things.

--- MOM.m
+++ MOM.m
@@ -1,30 +1,30 @@
 %% Project #3 MOM
 % Authors: Dayo Lawal and Blake Levy
-clc;clear;
+clc;clear;clf;
 Q = [1]; % q-point Quadrature
 for G = 1:length(Q)
     q = Q(G);
     constants
-    tol = 1e-10; % Tolerance for GMRES
-    M = 5*160; % Number of elements
+    tol = 1e-11; % Tolerance for GMRES
+    M = ceil(10*lamb0); % Number of elements
     N = M+1; % Number of nodes
     r = pi*lamb0; % radius of circle
     [x y theta S slope L] = mesh_circle(M,r);
     % [P] = testpts(x(5),y(5),S(:,5),q);
     %% Z, V and I
-    theta1 = 180*pi/180;
+    theta1 = 0*pi/180;
     Hzo = 1;
     V = zeros(M,1);
 %     [V, m] = create_input(x,y,k0, theta1, Ez0);
     % plot(x,y,'r',P(1),P(2),'+k',m(1,:),m(2,:),'ko')
-    Z = 1/2*eye(M,M);
+    Z = -1/2*eye(M,M);
     R = zeros(2,length(x));
     R(1,:) = x;
     R(2,:) = y;
     for i = 1:M
         for j = 1:M
             if (i ~= j)
-                Z(i,j) = (k0/4/1j)*create_Z(k0, S(:,i)/L(i), R(:,i), R(:,j),S(:,i),S(:,j),q);
+                Z(i,j) = (k0/4/1j)*create_Z_notes(k0, S(:,i)/L(i), R(:,i), R(:,j),S(:,i),S(:,j),q);
             end  
         end
         V(i) = create_input(k0,theta1,Hzo,R(:,i),S(:,i),q);
--- geometry.m
+++ geometry.m
@@ -2,7 +2,7 @@
 % graphs geometry used for MOM calculations
 clc;clear;
 constants
-M = 5*160;
+M = 4;
 r = 1;
 q = 1;
 [x y theta S slope L ] = mesh_circle(M,r);
@@ -10,5 +10,13 @@ P = zeros(2,q*length(x)-q);
 for i = 1:length(x)-1
     P(:,[q*i-(q-1):q*i]) = testpts( x(i), y(i), S(:,i), q);
 end
+angle = zeros(1,length(S));
+for i = 1:length(S)
+     if S(1,i)*S(2,i) >= 0 
+        angle(i) = atan2(S(2,i),S(1,i));
+     else
+         angle(i) = atan2(S(2,i),S(1,i));
+     end
+ end
 plot(x,y,'r',P(1,:),P(2,:),'ko')
 title(strcat(int2str(M),' Elements with ', int2str(q),' Quadrature Points'));
