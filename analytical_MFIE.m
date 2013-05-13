998e40a163eb87d92772ba57322496af8c6c4c14
Blake Levy <blake.a.levy@gmail.com>
2013-05-13 10:50:02 -0500

    Rebase

--- analytical_MFIE.m
+++ analytical_MFIE.m
@@ -3,23 +3,29 @@ function [ J, E ] = analytical_MFIE(n,k0,r)
 constants
 %% current on PEC cylinder
 ka = k0*r;
-phi = 0:2*pi/(n-1):2*pi; % number of terms
+phi = 0:2*pi/n:2*pi; % number of terms
 J = 1j/(pi*ka*eta0)*ones(1,length(phi));
 E = 4/k0*ones(1,length(phi));
-k = 2*ones(1,n+1);
-k(1) = 1;
+k = [1 2];
 
 for t = 1:length(phi)
     angle = phi(t);
     sum = 0;
     sum_c = 0;
-    for i = 0:n
-          sum = sum + (1j)^(-i)*2*k(i+1)*cos(i*angle)/(hankel_prime(i,ka));
-          sum_c = sum_c + k(i+1)*bessel_prime(i,ka)*cos(i*angle)/hankel_prime(i,ka);
+    for i = 0:n-1
+          if i == 0
+              sum = sum + (1j)^(-i)*2*k(1)*cos(i*angle)/(hankel_prime(i,ka));
+              sum_c = sum_c + k(1)*bessel_prime(i,ka)*cos(i*angle)/hankel_prime(i,ka);
+          else
+              sum = sum + (1j)^(-i)*2*k(2)*cos(i*angle)/(hankel_prime(i,ka));
+              sum_c = sum_c + k(2)*bessel_prime(i,ka)*cos(i*angle)/hankel_prime(i,ka);
+          end
     end
     J(t) = J(t)*sum;
     E(t) = E(t)*abs(sum_c)^2;
 end
+J = J(1:end-1);
+E = E(1:end-1);
 % plot(phi*180/pi,abs(J))
 % xlim([phi(1)*180/pi phi(end)*180/pi])
 % %% Ratio of Scattered field to incident field
